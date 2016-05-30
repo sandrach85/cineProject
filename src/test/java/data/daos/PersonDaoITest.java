6b14d21ba4d47ce8cdc27da2fc8bc4d82f5df8e5
Sandra <sandrachacon@alumnos.upm.es>
2016-05-30 19:37:34 +0200

    Creados tes de PersonsDao y RoleUsedDao

---
+++ src/test/java/data/daos/PersonDaoITest.java
@@ -0,0 +1,30 @@
+package data.daos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import config.PersistenceConfig;
+import config.TestsPersistenceConfig;
+import data.entities.Person;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceConfig.class, TestsPersistenceConfig.class})
+public class PersonDaoITest {
+
+    @Autowired
+    private PersonDao personDao;
+    
+    @Autowired
+    private DaosService daosService;
+    
+    @Test
+    public void testFindById() {
+        Person person = personDao.findById(0);
+        assertEquals("Ben Affleck", person.getName());
+    }
+}
