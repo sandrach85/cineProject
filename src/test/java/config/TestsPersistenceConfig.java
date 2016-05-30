05e5ffd4b3b9993cc9487cbfb84b25c81d4411f6
Sandra <sandrachacon@alumnos.upm.es>
2016-05-30 18:29:17 +0200

    Añadida configuración para los test de persistencia

---
+++ src/test/java/config/TestsPersistenceConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {ResourceNames.DAOS, ResourceNames.SERVICES, ResourceNames.CONTROLLERS})
+public class TestsPersistenceConfig {
+
+}
