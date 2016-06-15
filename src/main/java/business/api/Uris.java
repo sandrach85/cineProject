0b5df7a1c1a3bfc869636e470227a50579969225
Sandra <sandrachacon@alumnos.upm.es>
2016-06-15 19:02:06 +0200

    Creadas URIs

---
+++ src/main/java/business/api/Uris.java
@@ -0,0 +1,13 @@
+package business.api;
+
+public class Uris {
+
+    public static final String SERVLET_MAP = "/api/v0";
+
+    public static final String FILMS = "/films";
+    
+    public static final String ACTORS = "/actors";
+    
+    public static final String DIRECTORS = "/directors";
+
+}
