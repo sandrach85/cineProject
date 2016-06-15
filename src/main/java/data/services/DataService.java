b9cb6d86a9e7843c6dfd41b361c0867a2b0b5d3d
Sandra <sandrachacon@alumnos.upm.es>
2016-06-15 18:06:47 +0200

    Eliminado roleUsed

--- src/main/java/data/services/DataService.java
+++ src/main/java/data/services/DataService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import data.daos.FilmDao;
 import data.daos.PersonDao;
-import data.daos.RoleUsedDao;
 import data.daos.ThemeUsedDao;
 
 @Service
@@ -15,17 +14,13 @@ public class DataService {
     
     @Autowired
     private PersonDao personDao;
-    
-    @Autowired
-    private RoleUsedDao roleUsedDao;
-    
+         
     @Autowired
     private ThemeUsedDao themeUsedDao;
     
     public void deleteAll() {
         filmDao.deleteAll();
         personDao.deleteAll();
-        roleUsedDao.deleteAll();
         themeUsedDao.deleteAll();
     }
 
